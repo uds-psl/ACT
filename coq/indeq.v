3348679b468a1d12e851568320b49c0eba9c4214
smolka <smolka@ps.uni-saarland.de>
2020-12-02 10:36:24 +0100

    blah

--- coq/indeq.v
+++ coq/indeq.v
@@ -22,8 +22,8 @@ Section UIP.
   Goal UIP -> K_Streicher.
   Proof.
     intros H x p a e.
-    destruct (H x x eq_refl e).
-    exact a.
+    specialize (H x x eq_refl e).
+    destruct H. exact a.
   Qed.
 
   Goal K_Streicher -> UIP'.
@@ -36,17 +36,42 @@ Section UIP.
     intros H p x a. apply H. reflexivity.
   Qed.
 
+  Goal CD -> UIP'.
+  Proof.  (* Idea from William, 28 Nov 2020 *)
+    intros H x.
+    enough (forall y  (e': y = x) (e: x = y), cast e' e = eq_refl x) as H'.
+    { apply (H' x (eq_refl x)). }
+    intros y e' e. destruct e. apply H.
+  Qed.
+
   Lemma cast_eq {x y: X} :
-    forall e: x = y, cast (p:= eq x) e (eq_refl x) = e.
+    forall e: x = y, cast e (eq_refl x) = e.
   Proof.
     destruct e. reflexivity.
   Qed.
 
   Goal CD -> UIP'.
-  Proof. (* Ideas from Dominik Kirst and Gaëtan Gilbert, 20 Nov 2020 *)
+  Proof. (* Idea from Gaëtan Gilbert, 20 Nov 2020 *)
     intros H x e. rewrite <-(cast_eq e). apply H.
   Qed.
 
+  Goal CD -> DPI.
+  Proof.
+    intros H p x.
+    enough (forall a b: sigT p, a = b -> forall e: pi1 a = pi1 b, cast e (pi2 a) = pi2 b) as H'.
+    { intros u v e'. apply (H' _ _ e' eq_refl). }
+    intros a b []. apply H.
+  Qed.
+
+  Goal DPI -> UIP'.
+  Proof.
+    intros H x e.
+    apply (H (fun z => z = x)). revert e.
+    enough (forall y (e: x = y), Sig (fun z => z = y) x e = Sig (fun z => z = y) y eq_refl) as H'.
+    { apply (H' x). }
+    intros y []. reflexivity.
+  Qed.
+ 
   Lemma DPI_eq1 {p: X -> Type} {a b: sigT p} :
     CD -> a = b -> forall e: pi1 a = pi1 b, cast e (pi2 a) = pi2 b.
   Proof.
@@ -126,5 +151,55 @@ Proof.
   apply Hedberg' with (f:= f).
   intros x y e e'. unfold f.
   destruct d as [e''|h]. reflexivity. destruct (h e).
+Qed.
+
+(* UIP propagates to identity at Type *)
+
+Inductive id X (x: X) : X -> Set := Q : id X x x.
+(* Must write Set, Type will be downgraded to Prop *)
+Arguments id {X}.
+Arguments Q {X}.
+
+Check id nat.
+Check id Type.
+
+Definition D {X} {x y: X}
+  : id x y -> x = y
+  := fun a => match a with Q _ => eq_refl x end.
+Definition U {X} {x y: X}
+  : x = y -> id x y
+  := fun e => match e with eq_refl _ => Q x end.
+Fact UD_eq {X} {x y: X} (a: id x y) :
+  U (D a) = a.
+Proof.
+  destruct a. reflexivity.
+Qed.
+Fact DU_eq {X} {x y: X} (e: x = y) :
+  D (U e) = e.
+Proof.
+  destruct e. reflexivity.
+Qed.
+
+Fact id_fun X Y (f: X -> Y) (x x': X) :
+  id x x' -> id (f x) (f x').
+Proof.
+  destruct 1. apply Q.
+Qed.
+
+Definition UIP_id X := forall (x y: X) (a b: id x y), id a b.
+Fact UIP_up X :
+  UIP X -> UIP_id X.
+Proof.
+  intros H x y a b.
+  rewrite <-(UD_eq a), <-(UD_eq b).
+  apply id_fun.
+  apply U. apply H.
+Qed.
+Fact UIP_down X :
+  UIP_id X -> UIP X.
+Proof.
+  intros H x y e e'.
+  rewrite <-(DU_eq e), <-(DU_eq e').
+  apply f_equal. apply D. apply H.
 Qed.
 
